a9316f766d956d9e4cb7e40c727cb6df59c805e8
Prasetyo Nur Hidayat <prasetyonurhidayat@gmail.com>
2022-01-10 07:54:48 +0700

    2nd

---
+++ IClientCallback.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+
+namespace WCFService_2Way_089
+{
+
+    [ServiceContract]
+    interface IClientCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void pesanKirim(string user, string pesan);
+
+    }
+}
